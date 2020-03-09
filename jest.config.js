639a2229d318b945ab71354712ad8337eafd3594
Attila Olah <NoNameProvided@users.noreply.github.com>
2020-03-09 13:44:40 +0100

    build: fix ts-jest deprecation warnings

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   collectCoverage: false,
   coverageDirectory: 'coverage',
-  mapCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.interface.ts", "!src/**/index.ts", "!**/node_modules/**"],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.interface.ts', '!src/**/index.ts', '!**/node_modules/**'],
   transform: {
-    '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest/preprocessor.js'
+    '^.+\\.ts$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: [ 'ts', 'js', 'json']
-}
+  moduleFileExtensions: ['ts', 'js'],
+};
